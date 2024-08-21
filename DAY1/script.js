c3f6cf319168fc04633dd8a617fada8cb58451ba
SAGDAFATHY <sagda.fathy.1995@gmail.com>
2024-08-21 18:44:46 +0300

    Day1

--- DAY1/script.js
+++ DAY1/script.js
@@ -1,18 +1,17 @@
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-window.addEventListener('keydown',function(e){
-    
-   const audio= document.querySelector(`audio[data-key="${e.keyCode}"]`);
-   if(!audio) return;
-   audio.currentTime =0;
-   audio.play();
-    const key= document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    key.classList.add(`playing`)
-      
-})
+    if (!audio) return; // Stop if no audio element is found
+    if (!key) return;   // Stop if no key element is found
 
-
-function removeTransition(e){
-    console.log(e)
-}
-const keys = document.querySelectorAll('.key')
-keys.forEach(ke => ke.addEventListener('transitionend',removeTransition))
+    audio.currentTime = 0; // Rewind the audio to the start
+    audio.play();
+    key.classList.add('playing');
+    setTimeout(
+        function () {
+            key.classList.remove('playing')
+        },
+        1
+    ) // Add playing class for the visual effect
+});
